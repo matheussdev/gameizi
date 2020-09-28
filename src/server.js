6c603a8aa5d1609b1d9a31b0cb2d6f1bcae6bf9a
matheus.souza2 <matheus.souza@acad.ifma.edu.br>
2020-09-28 09:44:54 -0300

    fix commit initial

--- src/server.js
+++ src/server.js
@@ -5,6 +5,13 @@ const EquipeController = require('./controllers/EquipeController');
 const paymentMethod = require
 const server = express();
 
+server.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
+  if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP 
+      res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS 
+  else //Se a requisição já é HTTPS 
+      next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado 
+});
+
 // SDK de Mercado Pago
 const mercadopago = require ('mercadopago');
 
